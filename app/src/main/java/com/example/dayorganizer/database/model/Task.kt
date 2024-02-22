84806c4b0a58b7db9c86f5fd5db0836673ec6526
mahhmoud refaat <mahhmoudrefat@outlook.com>
2024-02-22 21:05:07 +0200

    add : main page design add : item design

--- app/src/main/java/com/example/dayorganizer/database/model/Task.kt
+++ app/src/main/java/com/example/dayorganizer/database/model/Task.kt
@@ -1,17 +1,18 @@
 package com.example.dayorganizer.database.model
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity
 data class Task(
-    @PrimaryKey(autoGenerate = true) val id: Int? = null,
-    @ColumnInfo var title: String? = null ,
-    @ColumnInfo var content: String? = null ,
-    @ColumnInfo var isDone: Boolean = false ,
-    @ColumnInfo var dateTime: Long? = null ,
-
-
-
-    )
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,//unique value
+    @ColumnInfo var title: String? = null,
+    @ColumnInfo var content: String? = null,
+    @ColumnInfo var isDone: Boolean = false,
+    @ColumnInfo var dateTime: Long? = null,
+) : Parcelable
