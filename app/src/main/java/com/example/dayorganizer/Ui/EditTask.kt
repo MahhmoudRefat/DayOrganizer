7030bbab171f7d44ccc0185183c554ab4261b4f1
mahhmoud refaat <mahhmoudrefat@outlook.com>
2024-02-23 23:07:04 +0200

    add : Edit task Activity

---
+++ app/src/main/java/com/example/dayorganizer/Ui/EditTask.kt
@@ -0,0 +1,12 @@
+package com.example.dayorganizer.Ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dayorganizer.R
+
+class EditTask : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_task)
+    }
+}
