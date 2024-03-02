6024559308065bbab82c5334060d54b26892b308
mahhmoud refaat <mahhmoudrefat@outlook.com>
2024-03-02 20:06:20 +0200

    add :  edit task

--- app/src/main/java/com/example/dayorganizer/Ui/home/editTask/EditTask.kt
+++ app/src/main/java/com/example/dayorganizer/Ui/home/editTask/EditTask.kt
@@ -1,7 +1,10 @@
 package com.example.dayorganizer.Ui.home.editTask
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import com.example.dayorganizer.R
 
 class EditTask : AppCompatActivity() {
@@ -9,4 +12,9 @@ class EditTask : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_task)
     }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        return super.onCreateView(name, context, attrs)
+    }
+
 }
