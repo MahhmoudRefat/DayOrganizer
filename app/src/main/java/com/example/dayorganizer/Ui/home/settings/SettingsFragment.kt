c66c272ec8fa5f6fc5718a48ce6f429614ab811d
mahhmoud refaat <mahhmoudrefat@outlook.com>
2024-03-12 12:07:15 +0200

    add :  app arabic translation

--- app/src/main/java/com/example/dayorganizer/Ui/home/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/dayorganizer/Ui/home/settings/SettingsFragment.kt
@@ -1,29 +1,66 @@
 package com.example.dayorganizer.Ui.home.settings
 
+
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.example.dayorganizer.R
+import com.example.dayorganizer.Ui.model.Constants
+import com.example.dayorganizer.databinding.FragmentSettingsBinding
+import java.util.Locale
 
 
 class SettingsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var binding: FragmentSettingsBinding
+    private lateinit var languageArrayAdapter: ArrayAdapter<String>
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun onResume() {
+        super.onResume()
+        // language
+        val languages = resources.getStringArray(R.array.language_array)
+        languageArrayAdapter =
+            ArrayAdapter(requireContext(), R.layout.drop_down_item, languages)
+        binding.autoCompleteTVLanguages.setAdapter(languageArrayAdapter)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    ): View {
+        binding = FragmentSettingsBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        changeLanguage()
+    }
+
+    private fun changeLanguage() {
+        binding.autoCompleteTVLanguages.setOnItemClickListener{
+            parent,_,position,_ ->
+            val getSelected = parent.getItemAtPosition(position)
+            if (getSelected == "العربيه"){
+                setLocal("ar")
+                Constants.isArabic = true
+            }
+        }
+    }
+    private fun setLocal(s: String) {
+        val local = Locale(s)
+        Locale.setDefault(local)
+        val configuration = Configuration(resources.configuration)
+        configuration.setLocale(local)
+
+        requireContext().resources.updateConfiguration(
+            configuration,
+            requireContext().resources.displayMetrics,
+        )
+        requireActivity().recreate()
     }
 
 }
--- app/src/main/java/com/example/dayorganizer/Ui/model/Constants.kt
+++ app/src/main/java/com/example/dayorganizer/Ui/model/Constants.kt
@@ -2,4 +2,6 @@ package com.example.dayorganizer.Ui.model
 
 object Constants {
     const val TASK_KAY = "Task Kay"
+    var isDark: Boolean? = false
+    var isArabic: Boolean? = false
 }
